f973d73933cce5df6b1340946a8a9c154f5206fc
CHIRAG GARG <126180543+chiri1234@users.noreply.github.com>
2024-08-20 23:28:47 +0530

    Adding functionality to the website
    Next.js application

---
+++ brewbudyy/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <footer className='bg-gray-900 text-white flex justify-center items-center px-4 h-16'>
+      <p className = 'text-center'> Copyright &copy; BrewBuddy - All rights reserved!</p>
+      </footer>
+  )
+}
+export default Footer
