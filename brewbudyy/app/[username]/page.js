0198918c808e0b147170e02af1592627b931e53d
CHIRAG GARG <126180543+chiri1234@users.noreply.github.com>
2024-10-08 10:18:41 +0530

    Username
    added the user whom you want to pay

--- brewbudyy/app/[username]/page.js
+++ brewbudyy/app/[username]/page.js
@@ -1,7 +1,21 @@
 import React from 'react'
 
-export const username = ({params}) => {
+const username = ({params}) => {
   return (
-    <div className = 'text-white'>{params.username}</div>
+    <>
+    {params.username}
+    <div className = 'cover w-full relative'>
+    <img className = 'object-cover w-full h-[350]'src="https://c10.patreonusercontent.com/4/patreon-media/p/campaign/1812988/7079b883e68a4fee992c3336035bf78a/eyJ3IjoxOTIwLCJ3ZSI6MX0%3D/3.png?token-time=1730678400&amp;token-hash=OYb4coeyGhUsTmmx12ZgswzJDJf6zMuaik8NCq0tpSo%3D" alt=""/>
+    <div className = 'absolute -bottom-12 right-[45%] border-black-3'>
+      
+    <img className = 'rounded-full'width = {100} height = {100} src="https://c10.patreonusercontent.com/4/patreon-media/p/campaign/1812988/4c8195a724e4436081a645ab00493f48/eyJoIjoxMDgwLCJ3IjoxMDgwfQ%3D%3D/1.jpg?token-time=1729641600&amp;token-hash=jaFTGYVS6TTCpLlm-Zt_97DnMwjLYuLjF2myxkTVdEQ%3D" alt="" data-tag="creator-public-page-avatar" class="sc-bdvvtL iFMLae"/>
+    </div>
+    
+    </div>
+    <div className = "info flex justify-center item-center my-24">
+      @{params.username}
+    </div>
+    </>
   )
 }
+export default username
--- brewbudyy/components/Navbar.js
+++ brewbudyy/components/Navbar.js
@@ -1,23 +1,27 @@
 "use client";
-//nextjs is a server component and it is not possible to use the client side code in the server side code 
+//nextjs is Link server component and it is not possible to use the client side code in the server side code 
 //so we need to use the client side code in the client side code only
-import React from 'react'
+import React,{useState} from 'react'
+
 import {useSession, signIn, signOut} from 'next-auth/react'
 import Link from "next/link";
+
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
 </style>
 export const Navbar = () => {
   const { data: session } = useSession()
-  if(session) {
-    return <>
-      Signed in as {session.user.email} <br/>
-      <button onClick={() => signOut()}>Sign out</button>
-    </>
-  }
+  const [showdropdown, setShowdropdown] = useState(false);
+  // if(session) {
+  //   return <>
+  //     Signed in as {session.user.email} <br/>
+  //     <button onClick={() => signOut()}>Sign out</button>
+  //   </>
+  // }
   return (
     <nav className='bg-gray-900 text-white flex justify-between items-center px-4 h-16'>
-      <div className="logo font-bold text-lg">BrewBuddy</div>
+      <div className="logo font-bold text-lg">
+        <Link href = {"/"}>BrewBuddy</Link></div>
     {/* <ul className = 'font-medium flex justify-between gap-4'>
       <li>Home</li>
       <li>About</li>
@@ -25,14 +29,39 @@ export const Navbar = () => {
       <li>Sign Up</li>
       <li>Login</li>
     </ul> */}
-    <div>
+    <div className = "relative">
+    
+      {session && <> <button onClick ={()=> setShowdropdown(!showdropdown)} id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className=" mx-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Welcome {session.user.email} <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+</svg>
+</button>
+<div id="dropdown" className={`z-10 ${showdropdown?"":"hidden"} absolute hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700`}>
+    
+    <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+      <li>
+        <Link href="/dashboard" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</Link>
+      </li>
+      <li>
+        <Link href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</Link>
+      </li>
+      <li>
+        <Link href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</Link>
+      </li>
+      <li>
+        <Link href = "#" onClick ={()=> signOut()} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</Link>
+      </li>
+    </ul>
+</div></>}
+      {session && <Link href = {"/dashboard"}><button className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Dashboard</button> </Link>}
+      {session && <button className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" onClick={()=>{signOut()}}>Logout</button>}
+      {!session &&
       <Link href = {"/login"}>
       <button
             type="button"
             className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
           > Login
 
-      </button></Link> 
+      </button></Link>}
     </div>
   </nav>
 
